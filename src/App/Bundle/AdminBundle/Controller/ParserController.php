bb124dd9371f53b28f0f7c23442426a90679c365
Anton Gubarev <gubarev.bizz@gmail.com>
2017-02-28 23:27:47 +0200

    added flash

--- src/App/Bundle/AdminBundle/Controller/ParserController.php
+++ src/App/Bundle/AdminBundle/Controller/ParserController.php
@@ -14,30 +14,14 @@ class ParserController extends Controller
         $form = $this->createForm(UploadProductType::class);
         $form->handleRequest($request);
 
-        $path = $this->getParameter('upload_dir') . '/../parser';
-        $file = 'http://image.etov.ua/storage/640x640/1/3/f/0/13f0a98a97337227e42c5f4765d6ca8b.jpg';
-//        file_put_contents($path . "/3223.jpg", fopen($file, 'r'));
-//        file_put_contents($path . '/dasas.jpg', file_get_contents($file));
-//        preg_match_all("/\.gif|jpg|jpeg|png/", $file, $extension);
-
-//        var_dump($extension[0][0]);
-//        $ch = curl_init($file);
-//        $fp = fopen($path . '/dasas.jpg', 'wb');
-//        curl_setopt($ch, CURLOPT_FILE, $fp);
-//        curl_setopt($ch, CURLOPT_HEADER, 0);
-//        curl_exec($ch);
-//        curl_close($ch);
-//        fclose($fp);
-
-//        exit;
-
         if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
             $file = $this->get('app_admin.parser.file');
             $fileName = $file->uploadFile($form->getData()['file']);
             $filePath = $file->getPathNameByFileName($fileName);
             $this->get('app_admin.parser')->parser($filePath);
             $this->get('app_shop_bundle.multi_currency')->refreshCurrency();
+
+            $this->addFlash('success', 'Products have been successfully added.');
         }
 
         return $this->render('AppAdminBundle:Admin/Page:parser.html.twig', [
