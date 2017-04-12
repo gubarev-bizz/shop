9a9f91fd46a76c21068c01c6c857cf68d1a4d7b2
Anton Gubarev <gubarev.bizz@gmail.com>
2017-04-12 18:02:44 +0300

    change

--- src/App/Bundle/ShopBundle/Form/ProductCountDTOType.php
+++ src/App/Bundle/ShopBundle/Form/ProductCountDTOType.php
@@ -4,6 +4,7 @@ namespace App\Bundle\ShopBundle\Form;
 
 use App\Bundle\ShopBundle\Entity\DTO\ProductCountDTO;
 use App\Bundle\ShopBundle\Entity\Product;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -12,6 +13,19 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductCountDTOType extends AbstractType
 {
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -19,8 +33,9 @@ class ProductCountDTOType extends AbstractType
                 'required' => true,
             ])
             ->add('product', EntityType::class, [
-                'class' => Product::class,
                 'required' => true,
+                'class' => 'AppShopBundle:Product',
+//                'choices' => $this->getProducts(),
             ])
         ;
     }
@@ -37,5 +52,20 @@ class ProductCountDTOType extends AbstractType
     public function getBlockPrefix()
     {
         return 'app_shop_bundle_product_count_dto';
+    }
+
+    /**
+     * @return array
+     */
+    private function getProducts()
+    {
+        $products = $this->em->getRepository('AppShopBundle:Product')->findAll();
+        $data = [];
+
+        foreach ($products as $product) {
+            $data[$product->getId()] = 'SKU: ' . $product->getCode() . '; Title: ' . $product->getTitle();
+        }
+
+        return $data;
     }
 }
